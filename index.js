348650dec51f2ad615a42ced6f40e3cff53db2cd
Aman Kashyap <112754413+aman1722@users.noreply.github.com>
2023-06-22 12:26:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: ["Back-End Developer", "Problem Solver"],
+      strings: ["Full Stack Developer", "Problem Solver"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["Back-End Developer", "Problem Solver"],
+      strings: ["Full Stack Developer", "Problem Solver"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
