1de3c0b8bcc33923e7a673fe20e646edf05fa62f
Aman Kashyap <112754413+aman1722@users.noreply.github.com>
2023-07-05 23:12:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
   
     $(".scroll-up-btn").click(function () {
       $("html").animate({ scrollTop: 0 });
-      // removing smooth scroll on slide-up button click
+      
       $("html").css("scrollBehavior", "auto");
     });
   
