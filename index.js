1df825e386b7ef30a6994f630137432ae36f6cab
Aman Kashyap <112754413+aman1722@users.noreply.github.com>
2023-07-05 23:12:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     });
   
     $("#nav-menu .menu li a").click(function () {
-      // applying again smooth scroll on menu items click
+ 
       $("html").css("scrollBehavior", "smooth");
     });
   
