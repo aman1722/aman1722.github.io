6cb485e459824387d162db76ae560e9e42a35267
Aman Kashyap <112754413+aman1722@users.noreply.github.com>
2023-07-05 23:11:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-      // sticky navbar on scroll script
+     
       if (this.scrollY > 20) {
         $("#nav-menu").addClass("sticky");
       } else {
         $("#nav-menu").removeClass("sticky");
       }
   
-      // scroll-up button show/hide script
+      
       if (this.scrollY > 500) {
         $(".scroll-up-btn").addClass("show");
       } else {
@@ -15,7 +15,7 @@ $(document).ready(function () {
       }
     });
   
-    // slide-up script
+  
     $(".scroll-up-btn").click(function () {
       $("html").animate({ scrollTop: 0 });
       // removing smooth scroll on slide-up button click
